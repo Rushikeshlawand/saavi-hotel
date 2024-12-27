c223ac4cb3938214fdb2b4df71405648f36ac450
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2024-12-27 13:28:59 +0530

    navbar updated

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import './navbar.css';
-import logo from '../../assets/logo.png'; // Update this path based on your actual image location.
+import React from 'react'
+import './navbar.css'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <img src={logo} alt="Saavi Hotels Logo" />
+    <nav className='navbar'>
+      <div className='logo'>
+        <img src={logo} alt='Saavi Hotels Logo' />
       </div>
-      <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="#">Our Hotels</a></li>
-        <li><a href="#">Offers & Packages</a></li>
-        <li><a href="#">Contact Us</a></li>
+      <ul className='nav-links'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>About Us</a>
+        </li>
+        <li>
+          <a href='#'>Our Hotels</a>
+        </li>
+        <li>
+          <a href='#'>Offers & Packages</a>
+        </li>
+        <li>
+          <a href='#'>Contact Us</a>
+        </li>
       </ul>
-      <button className="book-now">Book Now</button>
+      <button className='book-now'>Book Now</button>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
