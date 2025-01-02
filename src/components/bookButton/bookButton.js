1ab6a583adf94e2817c8b6dbc44b9e4fee6654e1
Rushikesh Shrimant Lawand <rushilawand0@gmail.com>
2025-01-02 15:08:46 +0530

    booknow section updated

--- src/components/bookButton/bookButton.js
+++ src/components/bookButton/bookButton.js
@@ -1,67 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import './bookButton.css';
-import buildingDesign from '../../assets/bookNow/buildingBlock1.png';
-import buildingDesign2 from '../../assets/bookNow/buildingBlock2.png';
-import BoyImage from '../../assets/bookNow/boyImage.png';
-import GirlImage from '../../assets/bookNow/girlImage.png';
-import Salman1 from '../../assets/bookNow/salman1.png';
-import Salman2 from '../../assets/bookNow/salman2.png';
+import React, { useState, useEffect } from 'react'
+import './bookButton.css'
+import buildingDesign from '../../assets/bookNow/buildingBlock1.png'
+import buildingDesign2 from '../../assets/bookNow/buildingBlock2.png'
+import BoyImage from '../../assets/bookNow/boyImage.png'
+import GirlImage from '../../assets/bookNow/girlImage.png'
+import SecondImage1 from '../../assets/bookNow/SeconeImage.png'
+import SecondImage2 from '../../assets/bookNow/secondImage1.png'
 
 const BookButton = () => {
-  const [showSalmanImages, setShowSalmanImages] = useState(false);
+  const [showSalmanImages, setShowSalmanImages] = useState(false)
 
   useEffect(() => {
     // Set up an interval to toggle the images every 5 minutes (300,000 ms)
     const interval = setInterval(() => {
-      setShowSalmanImages((prev) => !prev); // Toggle the state
-    }, 2000);
+      setShowSalmanImages(prev => !prev) // Toggle the state
+    }, 2000)
 
     // Cleanup the interval when the component unmounts
-    return () => clearInterval(interval);
-  }, []);
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div className='forEmptyspace'>
-    <div className='background'>
-      <img
-        src={buildingDesign}
-        alt='Building Design'
-        className='background-image'
-      />
-      <img
-        src={buildingDesign2}
-        alt='Building Design'
-        className='background-image2'
-      />
-      {/* Conditional rendering for boy and girl images or Salman images */}
-      {showSalmanImages ? (
-        <>
-          {/* Salman images */}
-          <img src={Salman1} alt='Salman 1' className='boy-image' />
-          <img src={Salman2} alt='Salman 2' className='girl-image' />
-        </>
-      ) : (
-        <>
-          {/* Boy and Girl images */}
-          <img src={BoyImage} alt='Boy' className='boy-image' />
-          <img src={GirlImage} alt='Girl' className='girl-image' />
-        </>
-      )}
+      <div className='background'>
+        <img
+          src={buildingDesign}
+          alt='Building Design'
+          className='background-image'
+        />
+        <img
+          src={buildingDesign2}
+          alt='Building Design'
+          className='background-image2'
+        />
+        {/* Conditional rendering for boy and girl images or Salman images */}
+        {showSalmanImages ? (
+          <>
+            {/* Salman images */}
+            <img src={SecondImage1} alt='Salman 1' className='boy-image' />
+            <img src={SecondImage2} alt='Salman 2' className='girl-image' />
+          </>
+        ) : (
+          <>
+            {/* Boy and Girl images */}
+            <img src={BoyImage} alt='Boy' className='boy-image' />
+            <img src={GirlImage} alt='Girl' className='girl-image' />
+          </>
+        )}
 
-      <div className='content'>
-        <div className='bookText'>
-          <h4>Book Your</h4>
-          <div>
-            <h1>Dream Vacation Now</h1>
+        <div className='content'>
+          <div className='bookText'>
+            <h4>Book Your</h4>
+            <div>
+              <h1>Dream Vacation Now</h1>
+            </div>
           </div>
-        </div>
-        <div className='book-button'>
-          <button className='book-now-btn'>Book Now</button>
+          <div className='book-button'>
+            <button className='book-now-btn'>Book Now</button>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-  );
-};
+  )
+}
 
-export default BookButton;
+export default BookButton
